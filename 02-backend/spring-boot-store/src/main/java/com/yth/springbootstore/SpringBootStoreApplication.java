63a85841aaad561c0dd04dd3cad01e209003b161
Yuntian He <hytian994@gmail.com>
2023-05-21 22:45:55 -0400

    Initialize Spring boot

---
+++ 02-backend/spring-boot-store/src/main/java/com/yth/springbootstore/SpringBootStoreApplication.java
@@ -0,0 +1,13 @@
+package com.yth.springbootstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStoreApplication.class, args);
+	}
+
+}
