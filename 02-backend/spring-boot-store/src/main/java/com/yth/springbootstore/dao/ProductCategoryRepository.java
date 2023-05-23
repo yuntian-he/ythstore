5f48a1c8a92a91afc675c8c33d1a6ac6f09ecb65
Yuntian He <hytian994@gmail.com>
2023-05-22 21:04:28 -0400

    add products and products category

---
+++ 02-backend/spring-boot-store/src/main/java/com/yth/springbootstore/dao/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.yth.springbootstore.dao;
+
+import com.yth.springbootstore.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
