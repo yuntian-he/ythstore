2fa52724dec33831eda7c38015f4f990a18bbb01
Yuntian He <hytian994@gmail.com>
2023-05-29 23:27:57 -0400

    Deploy on AWS

--- 03-frontend/angular-ythstore/src/environments/environment.ts
+++ 03-frontend/angular-ythstore/src/environments/environment.ts
@@ -5,7 +5,7 @@ interface Environment {
 }
 export const environment = {
   production: true,
-  // shopApiUrl: "https://api.ythstore.com/api",
-  shopApiUrl: "https://localhost:8443/api",
+  shopApiUrl: "https://api.ythstore.com/api",
+  // shopApiUrl: "https://localhost:8443/api",
   stripePublishableKey: <string>"pk_test_51N8XmYGmKieVG20yYrcR3y478nfj2gfaNeJNxEPRxFOIp3xtKugweZOGaEw04MC6UB4wCbk8kymw5iw9dKhwxfgm00quWWBsfG"
 };
