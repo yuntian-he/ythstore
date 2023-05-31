705d391f6a4c60c031b5d8310e2683c4b705a9fa
Yuntian He <hytian994@gmail.com>
2023-05-30 22:46:27 -0400

    delete sensitive information

--- 03-frontend/angular-ythstore/src/environments/environment.prod.ts
+++ 03-frontend/angular-ythstore/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 interface Environment {
   production: boolean;
-  luv2shopApiUrl: string;
+  shopApiUrl: string;
   stripePublishableKey: string;
 }
 export const environment = {
   production: true,
   shopApiUrl: "https://ythstore.com/api",
   // shopApiUrl: "https://localhost:8443/api",
-  stripePublishableKey: <string>"pk_test_51N8XmYGmKieVG20yYrcR3y478nfj2gfaNeJNxEPRxFOIp3xtKugweZOGaEw04MC6UB4wCbk8kymw5iw9dKhwxfgm00quWWBsfG"
+  stripePublishableKey: <string>""
 };
